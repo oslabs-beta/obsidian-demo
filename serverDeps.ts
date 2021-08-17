00a21fbe179ebbc4bbf5bc159d6bb3070b7c91aa
cjamesb <cjberry1234@sbcglobal.net>
2021-08-17 16:34:20 -0700

    pushing to see new functionality on aws

--- serverDeps.ts
+++ serverDeps.ts
@@ -5,6 +5,7 @@ import {
   send,
 } from 'https://deno.land/x/oak@v6.0.1/mod.ts';
 
-import { ObsidianRouter, gql } from 'https://deno.land/x/obsidian/mod.ts';
+// import { ObsidianRouter, gql } from 'https://deno.land/x/obsidian/mod.ts';
+import { ObsidianRouter, gql } from 'https://raw.githubusercontent.com/open-source-labs/obsidian/master/mod.ts';
 import { Cron } from 'https://deno.land/x/crontab/cron.ts';
 export { Application, Router, Context, send, ObsidianRouter, gql, Cron };
