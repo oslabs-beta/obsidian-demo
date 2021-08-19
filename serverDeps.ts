156f6144224d7b1290ad54b8bce4aefa02a06ae9
cjamesb <cjberry1234@sbcglobal.net>
2021-08-19 05:06:54 -0700

    aws fixed?

--- serverDeps.ts
+++ serverDeps.ts
@@ -7,7 +7,7 @@ import {
 
 // import { ObsidianRouter, gql } from '../../obsidian/mod.ts'
 // import { ObsidianRouter, gql } from "https://raw.githubusercontent.com/oslabs-beta/obsidian/dev/mod.ts" //this is mode from raw dev mod.ts
-import { ObsidianRouter, gql } from 'https://deno.land/x/obsidian/mod.ts';
+import { ObsidianRouter, gql } from 'https://deno.land/x/obsidian@v3.2.1.1/mod.ts';
 
 //'https://deno.land/x/obsidian/mod.ts';
 import { Cron } from 'https://deno.land/x/crontab/cron.ts';
