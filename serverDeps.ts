6e525fa5f13c0ca58980b2155427bc4597a79f6d
cjamesb <cjberry1234@sbcglobal.net>
2021-08-19 04:47:01 -0700

    going to aws

--- serverDeps.ts
+++ serverDeps.ts
@@ -6,8 +6,8 @@ import {
 } from 'https://deno.land/x/oak@v6.0.1/mod.ts';
 
 // import { ObsidianRouter, gql } from '../../obsidian/mod.ts'
-import { ObsidianRouter, gql } from "https://raw.githubusercontent.com/oslabs-beta/obsidian/dev/mod.ts" //this is mode from raw dev mod.ts
-// import { ObsidianRouter, gql } from 'https://deno.land/x/obsidian/mod.ts';
+// import { ObsidianRouter, gql } from "https://raw.githubusercontent.com/oslabs-beta/obsidian/dev/mod.ts" //this is mode from raw dev mod.ts
+import { ObsidianRouter, gql } from 'https://deno.land/x/obsidian/mod.ts';
 
 //'https://deno.land/x/obsidian/mod.ts';
 import { Cron } from 'https://deno.land/x/crontab/cron.ts';
